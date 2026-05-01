231071a63302e5423b6b59e35fa34ee80ef568f7
Diego Pirovano <139859822+DiegoPirovanoIT@users.noreply.github.com>
2026-05-01 18:42:53 +0200

    aggiunto global window

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/PokerTrainer2/', // Es: '/mio-sito-react/'
+  base: '/PokerTrainer2/',
+  define: {
+    global: 'window', // Questo risolve l'errore "global" in modo corretto per Vite
+  },
   plugins: [react()],
 })
